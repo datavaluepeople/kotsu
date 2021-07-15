5085874fdea8098085a80804fabaaf8d72271635
dbcerigo <dbcerigo@gmail.com>
2021-07-15 11:11:11 +0100

    Implement test_registration for Entity

--- tests/test_registration.py
+++ tests/test_registration.py
@@ -1,17 +1,15 @@
 import pytest
 
-from truman import agent_registration
+from kotsu import registration
 
 
-def fake_agent_factory(env, param_1, param_2):
-    assert env == "FAKE_ENVIRONMENT"
+def fake_entity_factory(param_1, param_2):
     assert param_1 == "PARAM_1"
     assert param_2 == "PARAM_2"
 
 
-class FakeAgent:
-    def __init__(self, env, param_1, param_2):
-        assert env == "FAKE_ENVIRONMENT"
+class FakeEntity:
+    def __init__(self, param_1, param_2):
         assert param_1 == "PARAM_1"
         assert param_2 == "PARAM_2"
 
@@ -19,68 +17,75 @@ class FakeAgent:
 @pytest.mark.parametrize(
     "entry_point",
     [
-        fake_agent_factory,
-        FakeAgent,
-        "tests.test_agent_registration:fake_agent_factory",
-        "tests.test_agent_registration:FakeAgent",
+        fake_entity_factory,
+        FakeEntity,
+        "tests.test_registration:fake_entity_factory",
+        "tests.test_registration:FakeEntity",
     ],
 )
 def test_registration(entry_point):
-    registry = agent_registration.AgentRegistry()
+    registry = registration.EntityRegistry()
 
     registry.register(
-        id="SomeAgent-v0",
+        id="SomeEntity-v0",
         entry_point=entry_point,
         kwargs={"param_1": "PARAM_1", "param_2": "PARAM_2"},
+    )
+
+    registry.register(
+        id="SomeEntity-v1",
+        entry_point=entry_point,
+        kwargs={"param_1": "WRONG_PARAM_1", "param_2": "PARAM_2"},
     )
 
     # We can't use mocks, since we want to test importing via strings
     # So we run the asserts in the function
     # But we protect against the function never being run (and the test silently passing)
-    # by also asserting that there _is_ a failure if we run with the wrong environment
-    registry.make(id="SomeAgent-v0", env="FAKE_ENVIRONMENT")
+    # by also asserting that there _is_ a failure if we run with the wrong parameters
+    registry.make(id="SomeEntity-v0")
     with pytest.raises(AssertionError):
-        registry.make(id="SomeAgent-v0", env="WRONG_ENVIRONMENT")
+        registry.make(id="SomeEntity-v1")
 
     all_specs = list(registry.all())
-    assert len(all_specs) == 1
-    assert all_specs[0].id == "SomeAgent-v0"
+    assert len(all_specs) == 2
+    assert all_specs[0].id == "SomeEntity-v0"
+    assert all_specs[1].id == "SomeEntity-v1"
 
 
 def test_register_duplicate_id():
-    registry = agent_registration.AgentRegistry()
+    registry = registration.EntityRegistry()
 
-    registry.register("Agent-v0")
+    registry.register("Entity-v0")
     with pytest.raises(ValueError, match=r"Cannot re-register ID"):
-        registry.register("Agent-v0")
+        registry.register("Entity-v0")
 
 
-def test_make_missing_agent():
-    registry = agent_registration.AgentRegistry()
+def test_make_missing_entity():
+    registry = registration.EntityRegistry()
 
-    with pytest.raises(KeyError, match=r"No registered agent with ID"):
-        registry.make("Agent-v0")
+    with pytest.raises(KeyError, match=r"No registered entity with ID"):
+        registry.make("Entity-v0")
 
 
-def test_make_deprecated_agent():
-    registry = agent_registration.AgentRegistry()
+def test_make_deprecated_entity():
+    registry = registration.EntityRegistry()
 
-    registry.register("Agent-v0", entry_point=None)
-    with pytest.raises(ValueError, match=r"Attempting to make deprecated agent"):
-        registry.make("Agent-v0")
+    registry.register("Entity-v0", entry_point=None)
+    with pytest.raises(ValueError, match=r"Attempting to make deprecated entity"):
+        registry.make("Entity-v0")
 
 
 @pytest.mark.parametrize(
     "bad_id",
     [
-        "AgentMissingVersionName",
-        "AgentNoDashBetweenVersionv0",
-        "user1/user2/AgentTwoUsers-v0",
-        "AgentNaughty$ymbols-v0",
+        "EntityMissingVersionName",
+        "EntityNoDashBetweenVersionv0",
+        "user1/user2/EntityTwoUsers-v0",
+        "EntityNaughty$ymbols-v0",
     ],
 )
-def test_malformed_agent_id(bad_id):
-    registry = agent_registration.AgentRegistry()
+def test_malformed_entity_id(bad_id):
+    registry = registration.EntityRegistry()
 
-    with pytest.raises(ValueError, match=r"Attempted to register malformed agent ID"):
+    with pytest.raises(ValueError, match=r"Attempted to register malformed entity ID"):
         registry.register(bad_id)
