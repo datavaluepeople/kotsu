4cb2ff55b163415b1435f2e8ab84b1dd0635d496
dbcerigo <dbcerigo@gmail.com>
2021-07-31 17:29:27 +0100

    Implement typing module
    Defines core types, mainly for documentation purposes.

---
+++ kotsu/typing.py
@@ -0,0 +1,21 @@
+"""Define types and protocols for core interfaces for kotsu.
+
+Mainly for documentation purposes.
+"""
+from typing import Any, Callable, Dict, Union
+
+
+# The results from a validation run on a particular model.
+# Each Results dict will become one row of a combined results dataframe. Each key within the
+# Results dict will become a column of that dataframe, with values for matching keys across Results
+# put into the same matching column.
+# An example result from a 3-fold cross validation might be:
+# `{"average_score": _, "standard_deviation_scores": _, "fold_1_score": _, "fold_2_score"...}`
+Results = Dict[str, Union[float, str]]
+
+# Model's within kostu are not constrained in any way. It is up to the user to write Model's that
+# have an interface compatible with their Validations.
+Model = Any
+
+# A Validation is a callable (e.g. function) that takes a Model, and returns a Results.
+Validation = Callable[[Any], Results]
